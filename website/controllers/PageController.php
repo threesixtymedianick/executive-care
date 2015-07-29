3d1cb2c007050f8130267283eebca92d13f5e8ba
MatthewBooth <Kryten2k35@gmail.com>
2015-07-29 16:55:38 +0100

    Page: Contact us > Feature: Enquiry form
    Pivotal ID #100100350
    
    Added the enquiry form to the contact us page. Currently just sends a testing debug email to an email address at bolser (guser@bolser.co.uk) so we cna test it's working.
    
    sass probably wants tidying up at some point.

--- website/controllers/PageController.php
+++ website/controllers/PageController.php
@@ -1,6 +1,8 @@
 <?php
 
 use Website\Controller\PageController as AbstractPageController;
+use Website\Form\ContactUsForm as ContactUsForm;
+use Website\Form\BrochureForm as BrochureForm;
 
 class PageController extends AbstractPageController
 {
@@ -46,6 +48,48 @@ class PageController extends AbstractPageController
      */
     public function contactUsAction()
     {
+        $brochureForm = new BrochureForm();
 
+        $enquiryForm = new ContactUsForm();
+
+        $request = $this->getRequest();
+
+        if ($request->isPost()) {
+            // Create a new Zend View object
+            $view = new \Zend_View();
+
+            // Setup the location of our email templates
+            $view->setScriptPath('website/views/scripts/email');
+
+            // Create a new PimCore Mail object
+            $mail = new \Pimcore\Mail();
+
+            if ($brochureForm->isValid($request->getPost())) {
+                // Get the values from the form
+                $values = $brochureForm->getValues();
+
+                // Set the values to the view
+                //  in a variable called data
+                $view->data = $values;
+
+                // Send to a template
+                $html = $view->render('brochure.php');
+
+                // Add our email address for this form
+                $mail->addTo('guser@bolser.co.uk');
+
+
+
+            } else if ($enquiryForm->isValid($request->getPost())) {
+                $values = $enquiryForm->getValues();
+                $view->data = $values;
+                $html = $view->render('enquiry.php');
+                $mail->addTo('guser@bolser.co.uk');
+            }
+            $mail->setBodyHtml($html);
+            $mail->send();
+        }
+        $this->view->brochureForm = $brochureForm;
+        $this->view->enquiryForm = $enquiryForm;
     }
 }
---
+++ website/views/scripts/page/contact-us.php
@@ -0,0 +1,75 @@
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script>
+            // Wait until the DOM has loaded before querying the document
+            $(document).ready(function(){
+                $('ul.tabs').each(function(){
+                    // For each set of tabs, we want to keep track of
+                    // which tab is active and it's associated content
+                    var $active, $content, $links = $(this).find('a');
+
+                    // If the location.hash matches one of the links, use that as the active tab.
+                    // If no match is found, use the first link as the initial active tab.
+                    $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+                    $active.addClass('active');
+
+                    $content = $($active[0].hash);
+
+                    // Hide the remaining content
+                    $links.not($active).each(function () {
+                        $(this.hash).hide();
+                    });
+
+                    // Bind the click event handler
+                    $(this).on('click', 'a', function(e){
+                        // Make the old tab inactive.
+                        $active.removeClass('active');
+                        $content.hide();
+
+                        // Update the variables with the new link and content
+                        $active = $(this);
+                        $content = $(this.hash);
+
+                        // Make the tab active.
+                        $active.addClass('active');
+                        $content.show();
+
+                        // Prevent the anchor's default click action
+                        e.preventDefault();
+                    });
+                });
+            });
+        </script>
+<div class="contact-us">
+    <div class="contact-us__left">
+        <ul class="tabs">
+            <li><a href='#enquiry'>Make an enquiry</a></li>
+            <li><a href='#brochure'>Request a brochure</a></li>
+        </ul>
+        <div class="tab" id="enquiry">
+            <form enctype="application/x-www-form-urlencoded" action="" method="post">
+
+            <div class="tab__left">
+                <?= $this->enquiryForm->name ?>
+                <?= $this->enquiryForm->number ?>
+            </div>
+            <div class="tab__right">
+                <?= $this->enquiryForm->email ?>
+                <?= $this->enquiryForm->address ?>
+            </div>
+            <div class="tab__controls">
+                <?= $this->enquiryForm->message ?>
+                <?= $this->enquiryForm->opt_in ?>
+                <label for="opt_in">I would like to hear about the latest news and upcoming events</label>
+                <?= $this->enquiryForm->submit ?>
+            </div>
+        </form>
+        </div>
+        <div class="tab" id="brochure">
+            <?= $this->brochureForm; ?>
+        </div>
+    </div>
+
+    <br clear="all" />
+
+    LIVE CHAT HERE
+</div>
