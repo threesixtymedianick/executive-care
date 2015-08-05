c31b22359ad703d4d35d23f22d3b1624ee3abf29
tsuri511 <tsuri511@gmail.com>
2015-08-05 15:32:29 +0100

    Finished creating the copy, section accordian slider, find a home, open days, book a visit and recommendation panels.

--- src/js/modules/show_more_less.js
+++ src/js/modules/show_more_less.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
     $(".slide").hide();
+    $(".slide:first").css( "display", "block" );
+    $(".show_hide:first").text("Show Less -");
 
     $('.show_hide').click(function (e) {
         var slide_content = $(this).closest('.sliding_content').find('.slide');
-        slide_content.slideToggle("fast");
-        var val = $(this).text() == "Less -" ? "More +" : "Less -";
+        slide_content.slideToggle(500);
+        var val = $(this).text() == "Show Less -" ? "Show More +" : "Show Less -";
         $(this).hide().text(val).fadeIn("fast");
         e.preventDefault();
     });
