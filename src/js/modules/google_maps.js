4a88244e7c42da399e84f86a00839479adda493b
MatthewBooth <kryten2k35@ultimarom.com>
2015-08-05 16:02:43 +0100

    Finalised the map feature, second attempt!

--- src/js/modules/google_maps.js
+++ src/js/modules/google_maps.js
@@ -6,59 +6,58 @@ var placeLocation;
 function initializeGoogleMaps() {
     // The map object
     var map;
+
     // The window that shows when you click the marker
     var infowindow;
+
     // Our place ID
     var googlePlaceId = document.getElementById('map-canvas').getAttribute("data-placeId");
 
-    // Create a new map object in the "map-canvas" element
-    map = new google.maps.Map(document.getElementById('map-canvas'));
+    if (!(googlePlaceId === 'undefined' || googlePlaceId === null)) {
+        // Create a new map object in the "map-canvas" element
+        map = new google.maps.Map(document.getElementById('map-canvas'));
 
-    // The place ID for the care home
-    var request = {
-        placeId: googlePlaceId
-    };
+        // Maps info window
+        var infowindow = new google.maps.InfoWindow();
 
-    // Maps info window
-    var infowindow = new google.maps.InfoWindow();
+        // Get the places service
+        var service = new google.maps.places.PlacesService(map);
 
-    // Get the places service
-    var service = new google.maps.places.PlacesService(map);
+        // Place a marker on the map using the place we setup earlier
+        service.getDetails({ placeId:googlePlaceId }, function(place, status) {
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+                placeLocation = place;
 
-    // Place a marker on the map using the place we setup earlier
-    service.getDetails(request, function(place, status) {
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-            placeLocation = place;
+                // Center the map and set the zoom level
+                map.setOptions({
+                    center: placeLocation.geometry.location, // Center on the care home
+                    zoom: 16 // Zoom right in. Higher is more zoom
+                });
 
-            // Center the map and set the zoom level
-            map.setOptions({
-                center: placeLocation.geometry.location, // Center on the care home
-                zoom: 16 // Zoom right in. Higher is more zoom
-            });
+                // Place the marker
+                var marker = new google.maps.Marker({
+                    map: map,
+                    position: placeLocation.geometry.location
+                });
 
-            // Place the marker
-            var marker = new google.maps.Marker({
-                map: map,
-                position: placeLocation.geometry.location
-            });
+                // An onclick listener for the marker
+                google.maps.event.addListener(marker, 'click', function() {
+                    infowindow.setContent(place.name);
+                    infowindow.open(map, this);
+                });
+            }
+        });
 
-            // An onclick listener for the marker
-            google.maps.event.addListener(marker, 'click', function() {
-                infowindow.setContent(place.name);
-                infowindow.open(map, this);
-            });
-        }
-    });
+        // Create the DIV to hold the control and
+        // call the CenterControl() constructor passing
+        // in this DIV.
+        var viewOnGoogleDiv = document.createElement('div');
+        var centerControl = new openMapsInGoogleMaps(viewOnGoogleDiv);
 
-    // Create the DIV to hold the control and
-    // call the CenterControl() constructor passing
-    // in this DIV.
-    var viewOnGoogleDiv = document.createElement('div');
-    var centerControl = new openMapsInGoogleMaps(viewOnGoogleDiv);
-
-    // Place the control div
-    viewOnGoogleDiv.index = 1;
-    map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(viewOnGoogleDiv);
+        // Place the control div
+        viewOnGoogleDiv.index = 1;
+        map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(viewOnGoogleDiv);
+    }
 }
 
 /**
