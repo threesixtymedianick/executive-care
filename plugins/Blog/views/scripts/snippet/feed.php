684a330bdb4ad4353fe7d73adad5b17781be1173
MatthewBooth <kryten2k35@ultimarom.com>
2015-08-07 10:02:07 +0100

    Added pimcore blog extension as a git submodule

--- plugins/Blog/views/scripts/snippet/feed.php
+++
@@ -1,6 +0,0 @@
-<div class="snippet feed well">
-    <h3><?= $this->input('snippet-header') ?></h3>
-
-    <a href="/plugin/Blog/entry/feed">RSS</a> |
-    <a href="/plugin/Blog/entry/feed/format/atom">ATOM</a>
-</div>
