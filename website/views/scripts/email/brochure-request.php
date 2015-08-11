553ebcf37f9fb9e2be2654303fbd13886cdbac49
MatthewBooth <kryten2k35@ultimarom.com>
2015-08-11 10:44:22 +0100

    Moved around the email templates to match one and other

--- website/views/scripts/email/brochure-request.php
+++ website/views/scripts/email/brochure-request.php
@@ -1,6 +1,6 @@
 Care home: <?= $this->data['care_home_options']; ?> <br />
 Name: <?= $this->data['brochure_name']; ?> <br />
 E-mail: <?= $this->data['brochure_email']; ?><br />
-Address: <?= $this->data['brochure_address']; ?><br />
 Number: <?= $this->data['brochure_number']; ?><br />
+Address: <?= $this->data['brochure_address']; ?><br />
 Message: <?= $this->data['brochure_message']; ?><br />
--- website/views/scripts/email/enquiry.php
+++ website/views/scripts/email/enquiry.php
@@ -1,5 +1,6 @@
 Name: <?= $this->data['enquiry_name']; ?> <br />
 E-mail: <?= $this->data['enquiry_email']; ?><br />
 Number: <?= $this->data['enquiry_number']; ?><br />
-Address: <?= $this->data['enquiry_address']; ?><br /><br />
-Message: <?= $this->data['enquiry_message']; ?>
+Address: <?= $this->data['enquiry_address']; ?><br />
+Message: <?= $this->data['enquiry_message']; ?><br />
+
