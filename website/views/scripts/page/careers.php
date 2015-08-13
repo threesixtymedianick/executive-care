9ff2c226412a51f2cbfe4fa294caa931d21b7c39
MatthewBooth <kryten2k35@ultimarom.com>
2015-08-13 17:27:55 +0100

    Initial upload of download form. Requires apache mod_headers to be enabled, which was assigned to be so in ansible

--- website/views/scripts/page/careers.php
+++ website/views/scripts/page/careers.php
@@ -1,3 +1,4 @@
+<?php $asset = Asset::getById(25); ?>
 <div class="container">
     <div class="container__inner">
         <div class="careers">
@@ -7,6 +8,10 @@
             <div class="careers__right">
                 <div class="careers__right__applyonline">
                     <a href="/careers/apply" alt="Apply online now">Apply Online</a>
+                </div>
+
+                <div class="careers__right__download">
+                    <a href="<?= $asset->getFullPath() ?>" alt="Download application form">Download form</a>
                 </div>
             </div>
         </div>
