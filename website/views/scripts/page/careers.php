6f4cbee443d8a74f0cbcbd2100dc2150acfab1fb
MatthewBooth <kryten2k35@ultimarom.com>
2015-08-13 16:09:23 +0100

    Initial implementation of the apply online button
    There is no styling
    
    There is no dummy form

--- website/views/scripts/page/careers.php
+++ website/views/scripts/page/careers.php
@@ -1,1 +1,18 @@
-Blank Page
+<?php
+$aboutUsInfoTab       = $this->wysiwyg("about_us_info");
+$aboutUsTrainingTab   = $this->wysiwyg("about_us_training");
+?>
+<div class="container">
+    <div class="container__inner">
+        <div class="careers">
+            <div class="careers__left">
+                Left turn, Clyde.
+            </div>
+            <div class="careers__right">
+                <div class="careers__right__applyonline">
+                    <a href="/careers/apply" alt="Apply online now">Apply Online</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
