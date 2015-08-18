db2ab6f7f883e66fdc53ba55d9f1b297279cd199
MatthewBooth <kryten2k35@ultimarom.com>
2015-08-18 14:50:00 +0100

    Added auto scrolling to the carousel
    - Set to 8 seconds between slides
    - Mouse over a slide pauses the slider

--- src/js/modules/homepage_slider.js
+++ src/js/modules/homepage_slider.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
     $('.bxslider').bxSlider({
         adaptiveHeight: true,
         mode: 'fade',
-        slideWidth: 1000
+        slideWidth: 1000,
+        auto : true,
+        pause: 8000,
+        autoHover: true
     });
 });
