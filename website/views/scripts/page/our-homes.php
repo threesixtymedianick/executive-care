7ee361e06e51cd81fd4dcb3f89c2099412dfe48c
MatthewBooth <kryten2k35@ultimarom.com>
2015-08-18 17:26:09 +0100

    Initial commit of our homes template code
    TODO:
    
    - [ ] Add end user changeable code for the description part that is currently ipsum lorem
    - [ ] Fix the styling to be better laid out and more efficient

--- website/views/scripts/page/our-homes.php
+++ website/views/scripts/page/our-homes.php
@@ -2,6 +2,70 @@
     <div class="container__inner">
         <div class="our-homes">
             <div class="our-homes__left">
+                <div class="our-homes__left__title">
+                    Our Homes
+                </div>
+                <div class="our-homes__left__content">
+                    <p>
+                        Bacon ipsum dolor amet sausage shoulder doner ham, drumstick flank beef pastrami tail. Doner
+                        meatball turkey, jowl pastrami corned beef t-bone pancetta. Pancetta pastrami spare ribs, kevin
+                        meatball picanha cow shank porchetta. Spare ribs cupim tongue ground round bresaola tri-tip.
+                        Hamburger kevin salami, doner pancetta swine chuck jowl strip steak tenderloin tri-tip chicken
+                        turkey. Pastrami ground round turkey pork belly pancetta strip steak t-bone leberkas spare ribs
+                        cupim brisket.
+                    </p>
+                    <input type="search" placeholder="Search by name, town or postcode" />
+                    <!-- TODO: Here we will eventually loop over the database of homes and add them automatically for now, here are some default ones I've added -->
+                    <br style="clear: both;" />
+                    <div class="our-homes__left__content__homes-box-left">
+                        <img class="our-homes__left__content__homes-box-left__image"
+                             src="website/static/images/home/find-a-home.png" alt=""/>
+                        <div class="our-homes__left__content__homes-box-left__info">
+                            <div class="our-homes__left__content__homes-box-left__info__title">
+                                Fernedale<br />
+                                Care Home
+                            </div>
+                            <div class="our-homes__left__content__homes-box-left__info__distance">
+                                xx Miles away<br/>
+                            </div>
+                            <div class="our-homes__left__content__homes-box-left__info__address">
+                                Parksprings Road</br >
+                                Gainsborough<br/>
+                                Lincolnshire<br/>
+                            </div>
+                        </div>
+                        <div class="our-homes__left__content__homes-box-left__find-out-more">
+                            Find out more
+                        </div>
+
+                        <br style="clear:both" />
+                    </div>
+
+                    <div class="our-homes__left__content__homes-box-right">
+                        <img class="our-homes__left__content__homes-box-right__image"
+                             src="website/static/images/home/find-a-home.png" alt=""/>
+                        <div class="our-homes__left__content__homes-box-right__info">
+                            <div class="our-homes__left__content__homes-box-right__info__title">
+                                Fernedale<br />
+                                Care Home
+                            </div>
+                            <div class="our-homes__left__content__homes-box-right__info__distance">
+                                xx Miles away<br/>
+                            </div>
+                            <div class="our-homes__left__content__homes-box-right__info__address">
+                                Parksprings Road</br >
+                                Gainsborough<br/>
+                                Lincolnshire<br/>
+                            </div>
+                        </div>
+                        <div class="our-homes__left__content__homes-box-right__find-out-more">
+                            Find out more
+                        </div>
+
+                        <br style="clear:both" />
+                    </div>
+                    <br style="clear:both" />
+                </div>
             </div>
             <div class="our-homes__right">
             </div>
