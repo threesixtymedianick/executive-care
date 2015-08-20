a677a15b3ad0111bccd8747f30128b36052c6a82
MatthewBooth <kryten2k35@ultimarom.com>
2015-08-20 16:16:21 +0100

    Added full title bar clicking to change the content
    Clicking the whole title bar now changes the content inside, and updates the "show more" and "show less" text as well, as expected.

--- src/js/modules/show_more_less.js
+++ src/js/modules/show_more_less.js
@@ -3,11 +3,24 @@ $(document).ready(function () {
     $(".slide:first").css( "display", "block" );
     $(".show_hide:first").text("Show Less -");
 
-    $('.show_hide').click(function (e) {
+    $('.our-care__left__sliding__title').click(function (e) {
         var slide_content = $(this).closest('.sliding_content').find('.slide');
         slide_content.slideToggle(500);
-        var val = $(this).text() == "Show Less -" ? "Show More +" : "Show Less -";
-        $(this).hide().text(val).fadeIn("fast");
+
+        var showMore = "Show More +";
+        var showLess = "Show Less -";
+
+        var text = $(this).find('.show_hide').text();
+        console.log(text);
+
+        if (text === showMore) {
+            text = showLess;
+        } else {
+            text = showMore;
+        }
+
+        $(this).find('.show_hide').text(text);
+
         e.preventDefault();
     });
 });
