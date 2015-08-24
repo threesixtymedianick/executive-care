fc656729bbedc412b8be8448ca4d2736c1f28012
mattbradley91 <matthewbradley91@hotmail.co.uk>
2015-08-24 17:13:57 +0100

    Styled for mobile
    More styling to do on this page
    
    Quick spacing and indentation fixes
    
    And another...
    
    Renamed our care
    
    Removed unnecessary our-care.scss

--- website/views/scripts/page/our-care.php
+++ website/views/scripts/page/our-care.php
@@ -25,7 +25,9 @@
                 <?php while($this->block("contentblock")->loop()) : ?>
                     <div class="our-care__left__sliding sliding_content">
                         <div class="our-care__left__sliding__title">
+                        <div class="title1">
                             <?= $this->input("title"); ?>
+                            </div>
                             <?php if (!$this->editmode) : ?>
                                 <div class="our-care__left__sliding__title__show-hide">
                                     <a href="#" class="show_hide">Show More +</a>
