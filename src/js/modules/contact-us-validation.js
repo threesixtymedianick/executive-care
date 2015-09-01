53880fc5deb40537a61a097f8c5fd424195d52a6
MatthewBooth <kryten2k35@ultimarom.com>
2015-09-01 17:22:25 +0100

    Split out the volunbteer validation
    Renamed a lot of js files to use - to separate words

--- src/js/modules/contact-us-validation.js
+++ src/js/modules/contact-us-validation.js
@@ -47,26 +47,3 @@ $("#brochure_form").validate({
         brochure_address            : "Please enter an address to contact you with"
     }
 });
-
-$("#volunteer_form").validate({
-    rules: {
-        volunteer_name                : "required",
-        volunteer_number              : { required: true, number: true },
-        volunteer_address             : "required",
-        volunteer_message             : "required",
-        volunteer_email               : { required: true, email: true }
-    },
-    messages: {
-        volunteer_name                : "Please enter your name",
-        volunteer_number              : {
-            required: "Please enter your contact number",
-            number : "Please enter a valid number"
-        },
-        volunteer_email               : {
-            email: "Please enter a valid email address",
-            required: "An email address is required"
-        },
-        volunteer_message             : "Please enter a message to send",
-        volunteer_address             : "Please enter an address to contact you with"
-    }
-});
