a73da7e92bf9a23094c8e2f90c380a799757a1a7
MatthewBooth <kryten2k35@ultimarom.com>
2015-09-01 11:04:13 +0100

    Initial creation of the volunteer page

---
+++ website/views/scripts/page/volunteer.php
@@ -0,0 +1,15 @@
+<?php $careHomeObject = $this->careHomeObject; ?>
+
+<div class="our-care__header">-</div>
+<div class="container">
+    <div class="container__inner">
+        <div class="volunteer">
+            <div class="volunteer__left">
+                <div class="volunteer__left__content">
+
+                </div>
+                <div class="sidebar">
+                </div>
+            </div>
+        </div>
+    </div>
