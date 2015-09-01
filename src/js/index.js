5f1850ea7e7f70dad82de42ae35281e9731c8664
MatthewBooth <kryten2k35@ultimarom.com>
2015-09-01 17:18:41 +0100

    Revert "Rename form validation file"
    This reverts commit 8e645d1d8c270b377aec85199af38c988ee8235c.

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 require("./modules/tabs");
 require("./modules/homepage_slider");
 require("./modules/blog_slider");
-require("./modules/form_validation");
+require("./modules/contact-us_validation");
 require("./modules/google_maps");
 require("./modules/show_more_less");
 require("./modules/text_size_adjust");
