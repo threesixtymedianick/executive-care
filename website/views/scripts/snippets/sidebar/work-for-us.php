c3c6fcb211938463c8ded81a4a04971a0c151f9d
MatthewBooth <kryten2k35@ultimarom.com>
2015-09-01 10:30:38 +0100

    Added Work for us sidebar snippet and placed on the homepage

---
+++ website/views/scripts/snippets/sidebar/work-for-us.php
@@ -0,0 +1,14 @@
+<?php
+    $page = "workForUs";
+    $this->snippetEditModeStyles();
+    $title = $this->input($page . "Title");
+    $content = $this->textarea($page . "Content");
+    $linkTarget = $this->textarea($page . "LinkTarget");
+    $linkTitle = $this->textarea($page . "LinkTitle");
+?>
+
+<div class="home__panel home__panel--split home__panel--purple home__work-for-us">
+    <h3 class="home__panel-header"><?= $this->textarea("work-for-us"); ?></h3>
+    <p><?= $this->textarea("work-for-us-description"); ?></p>
+    <a class="home__button home__button--white" href="#">Read more</a>
+</div>
