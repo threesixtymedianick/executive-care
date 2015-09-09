203b0f9d7fe457cf70717d328957af4b8ff89dfa
Paul Clegg <paul+github@bolser.co.uk>
2015-09-09 13:00:41 +0100

    added Elastic Search seeder and updated event handler to save care home to ES

--- website/lib/Website/EventHandler/CareHomeEventHandler.php
+++ website/lib/Website/EventHandler/CareHomeEventHandler.php
@@ -1,6 +1,8 @@
 <?php
 namespace Website\EventHandler;
 
+use Elastica\Client;
+use Elastica\Document;
 use Geocoder\Exception\CollectionIsEmpty;
 use Geocoder\Model\Address;
 use Geocoder\Model\AddressCollection;
@@ -10,18 +12,35 @@ use Pimcore\Model\Object\CareHomes;
 class CareHomeEventHandler
 {
     /**
+     * Name of the Elastic Search index to save to
+     */
+    const ES_INDEX = 'carehomes';
+
+    /**
+     * Name of the Elastic Search type to save to
+     */
+    const ES_TYPE = 'home';
+
+    /**
      * @var Geocoder\Provider\LocaleAwareProvider
      */
     private $geocoder;
 
     /**
+     * @var Elastica\Client
+     */
+    private $client;
+
+    /**
      * Constructor
      *
      * @param Geocoder\Provider\LocaleAwareProvider $geocoder
+     * @param Elastica\Client $elasticSearch
      */
-    public function __construct(LocaleAwareProvider $geocoder)
+    public function __construct(LocaleAwareProvider $geocoder, Client $client)
     {
         $this->geocoder = $geocoder;
+        $this->client   = $client;
     }
 
     /**
@@ -65,7 +84,43 @@ class CareHomeEventHandler
         $object->setLat($address->getLatitude());
         $object->setLon($address->getLongitude());
 
+        // Update the ES index
+        $this->updateElasticSearchIndex($object);
+
         // Save the care home
         $object->save();
+    }
+
+    /**
+     * Update Elastic Search index
+     *
+     * @param  Pimcore\Model\Object\CareHomes $object
+     * @return
+     */
+    private function updateElasticSearchIndex($object)
+    {
+        $careHome = [
+            'id'      => $object->getId(),
+            'address' => [
+                'postcode' => $object->getPostcode(),
+            ],
+            'location' => [
+                'lat' => $object->getLat(),
+                'lon' => $object->getLon(),
+            ]
+        ];
+
+        // Create document
+        $careHomeDocument = new Document($object->getId(), $careHome);
+
+        // Get care home index
+        $careHomeIndex = $this->client->getIndex(self::ES_INDEX);
+
+        // Get care home type
+        $careHomeType = $careHomeIndex->getType(self::ES_TYPE);
+        $careHomeType->addDocument($careHomeDocument);
+
+        // Save
+        $careHomeType->getIndex()->refresh();
     }
 }
