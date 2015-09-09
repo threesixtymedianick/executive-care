b20a0bdb506058be7ed56bcacd741a3e6d713a6c
Paul Clegg <paul+github@bolser.co.uk>
2015-09-09 15:56:54 +0100

    only initalise our homes map on the our homes map page

--- src/js/features/our-homes-map.js
+++ src/js/features/our-homes-map.js
@@ -1,40 +1,41 @@
 $(document).ready(function() {
+    if ($('#our-homes .map').length > 0) {
+        // Google Map Options
+        var options = {
+            zoom: 7,
+            center: new google.maps.LatLng(53.7996388, -1.5491221),
+            disableDefaultUI: true,
+            zoomControl: true,
+            scrollwheel: false,
+            disableDoubleClickZoom: false
+        };
 
-    // Google Map Options
-    var options = {
-        zoom: 7,
-        center: new google.maps.LatLng(53.7996388, -1.5491221),
-        disableDefaultUI: true,
-        zoomControl: true,
-        scrollwheel: false,
-        disableDoubleClickZoom: false
-    };
+        // Initalise map
+        var map = new google.maps.Map(document.getElementById('map'), options);
 
-    // Initalise map
-    var map = new google.maps.Map(document.getElementById('map'), options);
+        // Get care home JSON
+        $.ajax({
+            url: '/ajax/care-homes',
+            type: 'GET',
+            dataType: 'json',
+        })
+        .done(function(data) {
+            $.each(data, function(key, data) {
+                // Check lat long is specified
+                if (data.lat !== null && data.lon !== null) {
+                    // Create LatLng
+                    var latLng = new google.maps.LatLng(data.lat, data.lon);
 
-    // Get care home JSON
-    $.ajax({
-        url: '/ajax/care-homes',
-        type: 'GET',
-        dataType: 'json',
-    })
-    .done(function(data) {
-        $.each(data, function(key, data) {
-            // Check lat long is specified
-            if (data.lat !== null && data.lon !== null) {
-                // Create LatLng
-                var latLng = new google.maps.LatLng(data.lat, data.lon);
-
-                // Create marker
-                var marker = new google.maps.Marker({
-                    position: latLng,
-                    map: map,
-                });
-            }
+                    // Create marker
+                    var marker = new google.maps.Marker({
+                        position: latLng,
+                        map: map,
+                    });
+                }
+            });
+        })
+        .fail(function() {
+            // Handle errors
         });
-    })
-    .fail(function() {
-        // Handle errors
-    });
+    }
 });
