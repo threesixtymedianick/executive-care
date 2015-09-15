49059dccd30ffa900418452c0cfe8138255b0c2c
MatthewBooth <kryten2k35@ultimarom.com>
2015-09-15 10:06:28 +0100

    Fixed blog title colour, should be independant of the testimonials now

--- website/views/scripts/blog/partial/testimonial-entry.php
+++ website/views/scripts/blog/partial/testimonial-entry.php
@@ -1,26 +1,26 @@
 <?php $entry = $this->entry; ?>
 
-<div class="blog__content__item">
-    <div class="blog__content__item--image">
+<div class="testimonials__content__item">
+    <div class="testimonials__content__item--image">
         <?php if ($entry->getBlogImage() !== null): ?>
             <img src="<?= $entry->getBlogImage()->getFullPath(); ?>" />
         <?php endif; ?>
     </div>
-    <div class="blog__content__item--summary">
-        <div class="blog__content__item--summary--title">
+    <div class="testimonials__content__item--summary">
+        <div class="testimonials__content__item--summary--title">
             <a href="<?= $this->url(['key' => $entry->getUrlPath()], 'blog-show', false, false); ?>">
                 <?= $entry->getTitle() ?>
             </a>
         </div>
         <?php if ($entry->getName() !== null): ?>
-            <div class="blog__content__item--summary--name">
+            <div class="testimonials__content__item--summary--name">
                 <?= $entry->getName(); ?>
             </div>
         <?php endif; ?>
-        <div class="blog__content__item--summary--date">
+        <div class="testimonials__content__item--summary--date">
             <?= $entry->getDate()->toString('dd.MM.Y'); ?>
         </div>
-        <div class="blog__content__item--summary-text">
+        <div class="testimonials__content__item--summary-text">
             <?= (trim($entry->getSummary()))
                 ? $entry->getSummary()
                 : Website_Tool_Text::cutStringRespectingWhitespace(trim(strip_tags($entry->getContent())), 200) ?>
