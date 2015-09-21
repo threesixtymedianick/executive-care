ea7504ce6550346fac9a5f10a1b0dbff4667490a
MatthewBooth <kryten2k35@ultimarom.com>
2015-09-21 13:03:40 +0100

    Added some css to handle when it's mobile

--- src/js/features/show-more-less.js
+++ src/js/features/show-more-less.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
     var minus = "-";
 
     function init() {
-        if (screen.width < 960) {
+        if (screen.width < 768) {
             showMoreText = plus;
             showLessText = minus;
             viewText = plus;
@@ -24,10 +24,15 @@ $(document).ready(function () {
             });
 
             $('.our-care__left__sliding__title__show-hide').each(function() {
+                var minHeight = $('.our-care__left__sliding__title').css("min-height");
+                console.log(minHeight);
                 $(this).css("padding", "0");
                 $(this).css("text-align", "center");
                 $(this).css("width", "50px");
                 $(this).css("font-size", "40px");
+                $(this).css("min-height", minHeight);
+                $(this).css("display", "table-cell");
+                $(this).css("vertical-align", "middle");
             });
             showHide.text(minus);
         } else {
