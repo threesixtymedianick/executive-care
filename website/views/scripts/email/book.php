5dd7f81d92b2e76112a55fd5ea91cfff651e98f0
MatthewBooth <kryten2k35@ultimarom.com>
2015-09-22 09:56:25 +0100

    Added email template for Book a visit

---
+++ website/views/scripts/email/book.php
@@ -0,0 +1,8 @@
+Care home: <?= $this->data['bookAVisit_careHomes']; ?> <br />
+Date: <?= $this->data['bookAVisit_date']; ?> <br />
+Day: <?= $this->data['bookAVisit_day']; ?><br />
+Time: <?= $this->data['bookAVisit_time']; ?><br />
+Name: <?= $this->data['bookAVisit_name']; ?><br />
+E-mail: <?= $this->data['bookAVisit_email']; ?><br />
+Number: <?= $this->data['bookAVisit_number']; ?><br />
+Address: <?= $this->data['bookAVisit_address']; ?><br />
