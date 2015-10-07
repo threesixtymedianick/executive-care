4e7dc52e423181a579953c58021439ab8a9af85b
MatthewBooth <kryten2k35@ultimarom.com>
2015-10-07 14:55:59 +0100

    Smaller fixes for this

--- website/views/scripts/page/home.php
+++ website/views/scripts/page/home.php
@@ -19,40 +19,7 @@ if ($this->editmode) : ?>
                 <div class="home__panel home__welcome">
                     <ul class="bxslider">
                         <?php foreach ($carouselImages as $element) : ?>
-                            <?php $image = $element->getThumbnail('homepage_slider');
-                            if (!($image === "" || $image === null) && $element instanceof \Pimcore\Model\Asset\Image) : ?>
-                                <?php
-                                $linkDocument = $element->getMetadata("Link-Document");
-                                $linkObject = $element->getMetadata("Link-Object");
-                                $linkExternal = $element->getMetadata("Link-External");
-                                if (null !== $linkDocument && "" !== $linkDocument) {
-                                    $link = $linkDocument;
-                                } elseif (null !== $linkObject && "" !== $linkObject) {
-                                    $link = $linkObject;
-                                } elseif (null !== $linkExternal && "" !== $linkExternal) {
-                                    $link = $linkExternal;
-                                } else {
-                                    $link = "";
-                                } ?>
-                                <a href="<?= $link; ?>">
-                                    <li style="background-image: url('<?= $image; ?>');">
-                                        <div class="bxslider__slider-caption">
-                                            <?php $metadataTitle = $element->getMetadata("title");
-                                            if (null !== $metadataTitle && "" !== $metadataTitle) : ?>
-                                                <h1>
-                                                    <?= $metadataTitle; ?>
-                                                </h1>
-                                            <?php endif; ?>
-                                            <?php $metadataSubtitle = $element->getMetadata("subtitle");
-                                            if (null !== $metadataSubtitle && "" !== $metadataSubtitle) : ?>
-                                                <h2>
-                                                    <?= $metadataSubtitle; ?>
-                                                </h2>
-                                            <?php endif; ?>
-                                        </div>
-                                    </li>
-                                </a>
-                            <?php endif; ?>
+                            <?= $this->partial("partial/homepage-slider.php", ['element' => $element]); ?>
                         <?php endforeach; ?>
                     </ul>
                 </div>
