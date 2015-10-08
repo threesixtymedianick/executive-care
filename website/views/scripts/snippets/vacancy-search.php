c8f066dc6150152854eaf854f7a3907cc9c73961
mattbradley91 <matthewbradley91@hotmail.co.uk>
2015-10-08 14:40:17 +0100

    Remove default drop down icons that appear on ie 9

--- website/views/scripts/snippets/vacancy-search.php
+++ website/views/scripts/snippets/vacancy-search.php
@@ -19,24 +19,28 @@ $vacancy = $this->vacancySelect();
     <div class="form-wrapper">
         <h5>Search by home:</h5>
         <form>
+        <div class="rmv-arrow-ie9">
             <select name="home_search" id="home_search">
                 <option value="">Select</option>
                 <?php foreach($careHomes as $option) : ?>
                     <option value="<?= $option[0]; ?>"><?= $option[1]; ?></option>
                 <?php endforeach; ?>
             </select>
+            </div>
         </form>
     </div>
     <div class="form-wrapper">
         <h5>Search by vacancy:</h5>
 
         <form>
+            <div class="rmv-arrow-ie9">
             <select name="vacancy_search" id="vacancy_search">
                 <option value="">Select</option>
                 <?php foreach($vacancy as $option) : ?>
                     <option value="<?= $option[0]; ?>"><?= $option[1]; ?></option>
                 <?php endforeach; ?>
             </select>
+            </div>
         </form>
     </div>
 </div>
