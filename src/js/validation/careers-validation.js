5816c53b75be1b5ae5a1c5544b57b30bdb78e10b
MatthewBooth <kryten2k35@ultimarom.com>
2015-10-09 11:17:09 +0100

    Initial upload of the new form

--- src/js/validation/careers-validation.js
+++ src/js/validation/careers-validation.js
@@ -2,82 +2,38 @@ $(document).ready(function () {
 
     var required = "*  - Required";
 
-    $(".application_form_fields").hide();
-    $(".application_form_fields:first").show();
-
     var form = "#application_form",
         rules = {
-        application_name                            : "required",
-        application_number                          : { required: true, number: true },
-        application_address                         : "required",
+        application_name                            : { required: true },
         application_email                           : { required: true, email: true, valDomain: true },
-        application_apply_reason                    : "required",
-        application_special_experience              : "required",
-        application_strengths                       : "required",
-        application_recent_company_name             : "required",
-        application_recent_company_position         : "required",
-        application_recent_company_address          : "required",
-        application_recent_company_number           : "required",
-        application_recent_company_email            : { required: true, email: true, valDomain: true },
-        application_recent_company_from_date        : "required",
-        application_recent_company_to_date          : "required",
-        application_reason_for_leaving              : "required",
-        application_agree_statement                 : "required",
-        application_signature                       : "required",
-        application_home_name_1                     : "required",
-        application_position_1                      : "required",
-        application_preferred_shift                 : "required",
-        application_preferred_hours                 : "required",
-        application_how_did_you_hear                : "required",
-        application_recent_company_email_one        : { valDomain: true },
-        application_recent_company_email_two        : { valDomain: true },
-        application_recent_company_email_three      : { valDomain: true }
-
-
-    };
+        application_number                          : { required: true, number: true },
+        application_careHomes                       : { required: true },
+        application_vacancyRole                     : { required: true },
+        application_coverLetter                     : { required: true }
+        };
 
     if($(form).length > 0) {
         var validator = $(form).validate({
             rules: rules
         });
     }
+});
 
-    $('.careers__apply__left__form__nextbutton').on('click', function (event) {
-        event.preventDefault();
-
-        var visible = $('.careers__apply__left__form').find('input[type=text], textarea, select').filter(":visible"),
-            checkbox = $('.careers__apply__left__form').find('input[type=checkbox]').filter(":visible"),
-            valid = 0;
-
-        // Get elements in sections
-        visible.each(function() {
-            // Validate each element
-            if($(this).valid()) {
-                valid++;
-            }
-        });
-
-        // Separate check for checkboxes
-        checkbox.each(function() {
-            // Validate each element
-            if($(this).valid()) {
-                valid++;
-            }
-        });
-
-        if ((visible.length + checkbox.length) === valid) {
-            nextFormPage($(this));
-        }
-    });
-
-    function nextFormPage (thisPage) {
-        $('.application_form_fields').fadeOut(600, function() {
-            thisPage.parent().next('.application_form_fields').fadeIn();
-        });
-    }
-
-    function previousFormPage () {
-        $(".application_form_fields").fadeOut();
-        $(this).parent().prev('.application_form_fields').fadeIn();
+$("#application_form").validate({
+    rules: {
+        application_name                            : { required: true },
+        application_email                           : { required: true, email: true, valDomain: true },
+        application_number                          : { required: true, number: true },
+        application_careHomes                       : { required: true },
+        application_vacancyRole                     : { required: true },
+        application_coverLetter                     : { required: true }
+    },
+    messages: {
+        application_name                            : required,
+        application_email                           : { required: required, email: "Please used an email address", valDomain: "Please enter a valid email address" },
+        application_number                          : required,
+        application_careHomes                       : required,
+        application_vacancyRole                     : required,
+        application_coverLetter                     : required
     }
 });
