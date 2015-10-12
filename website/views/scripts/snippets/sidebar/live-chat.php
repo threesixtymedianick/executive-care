f1195020437dc2e40295792612164c0d1222b832
MatthewBooth <kryten2k35@ultimarom.com>
2015-10-12 11:49:47 +0100

    Tidied up the snippet and make it look better when editing in the CMS

--- website/views/scripts/snippets/sidebar/live-chat.php
+++ website/views/scripts/snippets/sidebar/live-chat.php
@@ -1,8 +1,17 @@
+<?php if ($this->editmode) :
+    echo $this->snippetEditModeStyles();
+endif; ?>
+
 <?php $contactUsLiveChatDetails = $this->wysiwyg("live_chat_details_content", ["width" => 350, "height" => 100]); ?>
+
+
+<?php if ($this->editmode) : ?>
+<div style="width: 1200px;">
+<?php endif; ?>
 
 <div class="contact-us__live-chat">
     <div class="contact-us__live-chat__icon">
-        <img src="website/static/images/home/woman_headset2.png" />
+        <img src="/website/static/images/home/woman_headset2.png" />
     </div>
     <div class="contact-us__live-chat__details" style="margin-bottom:10px;">
         <div class="contact-us__live-chat__details__title">
@@ -16,3 +25,6 @@
         </div>
     </div>
 </div>
+<?php if ($this->editmode) : ?>
+    </div>
+<?php endif; ?>
