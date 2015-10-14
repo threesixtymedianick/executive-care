df9e47b970f996829b061c7c9a881f2b46d200df
tsuri511 <tsuri511@gmail.com>
2015-10-14 17:19:08 +0100

    changed background of png images, removed chat from contact page and altered the pgination size

--- website/views/scripts/page/contact-us.php
+++ website/views/scripts/page/contact-us.php
@@ -99,7 +99,6 @@
                         </div>
                     </form>
                 </div>
-                <?= $this->inc(Document_Snippet::getByPath('/snippets/live-chat')); ?>
             </div>
         </div>
     </div>
