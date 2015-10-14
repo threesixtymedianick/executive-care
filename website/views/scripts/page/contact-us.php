3033a75ea6e71c02a1e0ee4ea880fbb53369b84e
tsuri511 <tsuri511@gmail.com>
2015-10-14 17:19:08 +0100

    changed background of png images, removed chat from contact page and altered the pgination size

--- website/views/scripts/page/contact-us.php
+++ website/views/scripts/page/contact-us.php
@@ -99,22 +99,6 @@
                         </div>
                     </form>
                 </div>
-                <div class="contact-us__live-chat">
-                    <div class="contact-us__live-chat__icon">
-                        <img src="website/static/images/home/woman_headset2.png" />
-                    </div>
-                    <div class="contact-us__live-chat__details">
-                        <div class="contact-us__live-chat__details__title">
-                            Chat
-                        </div>
-                        <div class="contact-us__live-chat__details__content">
-                            <?= $contactUsLiveChatDetails ?>
-                        </div>
-                        <div class="contact-us__live-chat__details__open-chat">
-                            <a href="#livechat">Chat now</a>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     </div>
