8249efcdd95a6c3000cc4903c8257d80788bdb94
MatthewBooth <kryten2k35@ultimarom.com>
2015-10-27 15:29:38 +0000

    Fixed text adjustment javascript to ensure better compatibility with the CKEditor
    YouTrack: #execcare-272 work 1h

--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,7 @@ require("./modules/book-a-visit-form");
 require("./modules/contact-us-form");
 require("./modules/lightbox2-options");
 require("./modules/googleanalytics");
+require("./modules/wysiwyg-overrides");
 
 require("./features/show-more-less");
 require("./features/text-size-adjust");
---
+++ src/js/modules/wysiwyg-overrides.js
@@ -0,0 +1,1 @@
+CKEDITOR.config.fontSize_sizes = "Small/0.75em;Normal/1em;Large/1.125em;Larger/1.25em;";
