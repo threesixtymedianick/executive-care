cc7a9ffb295c28902c2d25a9300d2b6f77b64cdb
MatthewBooth <kryten2k35@ultimarom.com>
2015-10-27 15:42:43 +0000

    Revert "Wrap in document ready, for sanity"
    This reverts commit bbbfb7361d6ea726e49b708a190fdf0896409034.
    
    work 5m

--- src/js/modules/wysiwyg-overrides.js
+++ src/js/modules/wysiwyg-overrides.js
@@ -1,3 +1,1 @@
-$(document).ready(function() {
-    CKEDITOR.config.fontSize_sizes = "Small/0.75em;Normal/1em;Large/1.125em;Larger/1.25em;";
-});
+CKEDITOR.config.fontSize_sizes = "Small/0.75em;Normal/1em;Large/1.125em;Larger/1.25em;";
