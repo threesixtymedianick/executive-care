1447139a0fef0cd4867ece1f1e1ed11879cfbf42
MatthewBooth <kryten2k35@ultimarom.com>
2015-10-29 11:15:51 +0000

    Few more spacing corrections, just because
    YouTrack: #execcare-276 work 5m

--- website/views/scripts/email/application.php
+++ website/views/scripts/email/application.php
@@ -5,33 +5,34 @@
         <th colspan="2">Application for employment</th>
     </tr>
     <tr>
-        <td>Name: </td>
+        <td>Name:</td>
         <td><?= $this->data[$formName . 'name']; ?></td>
     </tr>
     <tr>
-        <td>Email: </td>
+        <td>Email:</td>
         <td><?= $this->data[$formName . 'email']; ?></td>
     </tr>
     <tr>
-        <td>Number: </td>
+        <td>Number:</td>
         <td><?= $this->data[$formName . 'number']; ?></td>
     </tr>
     <tr>
-        <td>Care home: </td>
+        <td>Care home:</td>
         <td><?= $this->data[$formName . 'careHomes']; ?></td>
     </tr>
     <tr>
-        <td>Role interested in: </td>
+        <td>Role interested in:</td>
         <td><?= $this->data[$formName . 'vacancyRoles']; ?></td>
     </tr>
     <tr>
-        <td>Covering letter: </td>
+        <td>Covering letter:</td>
         <td><?= str_replace("\n", "<br />", $this->data[$formName . 'coverLetter']); ?></td>
     </tr>
     <tr>
-        <td>CV Download: </td>
+        <td>CV Download:</td>
         <?php if ($this->data[$formName . 'cvFile']) : ?>
-            <td><a href="http://<?= $_SERVER['HTTP_HOST']; ?>/cv/<?= $this->data[$formName . 'cvFile']; ?>">Download attached CV</a></td>
+            <td><a href="http://<?= $_SERVER['HTTP_HOST']; ?>/cv/<?= $this->data[$formName . 'cvFile']; ?>">Download
+                    attached CV</a></td>
         <?php else : ?>
             <td>No CV attached</td>
         <?php endif; ?>
