fc5ec585225f07644723e282fd9333dc90997d85
MatthewBooth <kryten2k35@ultimarom.com>
2015-10-30 11:56:11 +0000

    Moved all favicon images to separate folders and added caching information to each item (to be able to force a change later_
    YouTrack: #execcare-277 work 1h30m

--- website/views/layouts/includes/head.php
+++ website/views/layouts/includes/head.php
@@ -6,22 +6,22 @@
   <link rel="stylesheet" href="/website/static/css/libraries.css" media="screen" charset="utf-8">
   <link rel="stylesheet" href="/website/static/css/style.css" media="screen" charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="apple-touch-icon" sizes="57x57" href="/website/static/images/apple-touch-icon-57x57.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="/website/static/images/apple-touch-icon-60x60.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="/website/static/images/apple-touch-icon-72x72.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="/website/static/images/apple-touch-icon-76x76.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="/website/static/images/apple-touch-icon-114x114.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="/website/static/images/apple-touch-icon-120x120.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="/website/static/images/apple-touch-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="/website/static/images/apple-touch-icon-152x152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="/website/static/images/apple-touch-icon-180x180.png">
-  <link rel="icon" type="image/png" href="/website/static/images/favicon-32x32.png" sizes="32x32">
-  <link rel="icon" type="image/png" href="/website/static/images/favicon-194x194.png" sizes="194x194">
-  <link rel="icon" type="image/png" href="/website/static/images/favicon-96x96.png" sizes="96x96">
-  <link rel="icon" type="image/png" href="/website/static/images/android-chrome-192x192.png" sizes="192x192">
-  <link rel="icon" type="image/png" href="/website/static/images/favicon-16x16.png" sizes="16x16">
-  <link rel="manifest" href="/website/static/images/manifest.json">
+  <link rel="apple-touch-icon" sizes="57x57" href="/website/static/images/favicon-images/apple/apple-touch-icon-57x57.png?v=M44L4fdbvp">
+  <link rel="apple-touch-icon" sizes="60x60" href="/website/static/images/favicon-images/apple/apple-touch-icon-60x60.png?v=M44L4fdbvp">
+  <link rel="apple-touch-icon" sizes="72x72" href="/website/static/images/favicon-images/apple/apple-touch-icon-72x72.png?v=M44L4fdbvp">
+  <link rel="apple-touch-icon" sizes="76x76" href="/website/static/images/favicon-images/apple/apple-touch-icon-76x76.png?v=M44L4fdbvp">
+  <link rel="apple-touch-icon" sizes="114x114" href="/website/static/images/favicon-images/apple/apple-touch-icon-114x114.png?v=M44L4fdbvp">
+  <link rel="apple-touch-icon" sizes="120x120" href="/website/static/images/favicon-images/apple/apple-touch-icon-120x120.png?v=M44L4fdbvp">
+  <link rel="apple-touch-icon" sizes="144x144" href="/website/static/images/favicon-images/apple/apple-touch-icon-144x144.png?v=M44L4fdbvp">
+  <link rel="apple-touch-icon" sizes="152x152" href="/website/static/images/favicon-images/apple/apple-touch-icon-152x152.png?v=M44L4fdbvp">
+  <link rel="apple-touch-icon" sizes="180x180" href="/website/static/images/favicon-images/apple/apple-touch-icon-180x180.png?v=M44L4fdbvp">
+  <link rel="icon" type="image/png" href="/website/static/images/favicon-images/web/favicon-32x32.png?v=M44L4fdbvp" sizes="32x32">
+  <link rel="icon" type="image/png" href="/website/static/images/favicon-images/web/favicon-194x194.png?v=M44L4fdbvp" sizes="194x194">
+  <link rel="icon" type="image/png" href="/website/static/images/favicon-images/web/favicon-96x96.png?v=M44L4fdbvp" sizes="96x96">
+  <link rel="icon" type="image/png" href="/website/static/images/favicon-images/android/android-chrome-192x192.png?v=M44L4fdbvp" sizes="192x192">
+  <link rel="icon" type="image/png" href="/website/static/images/favicon-images/web/favicon-16x16.png?v=M44L4fdbvp" sizes="16x16">
+  <link rel="manifest" href="/website/static/images/favicon-images/android/manifest.json?v=M44L4fdbvp">
   <meta name="msapplication-TileColor" content="#ffffff">
-  <meta name="msapplication-TileImage" content="/website/static/images/mstile-144x144.png">
+  <meta name="msapplication-TileImage" content="/website/static/images/favicon-images/mstile/mstile-144x144.png?v=M44L4fdbvp">
   <meta name="theme-color" content="#ffffff">
 </head>
